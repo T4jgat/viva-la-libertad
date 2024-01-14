46a718e628973abe3c74a5fa24e34cae1c07fe31
t4jgat <tmtmtm1423@gmail.com>
2024-01-15 02:21:06 +0600

    first commit

---
+++ src/main/java/kz/t4jgat/vivalalibertad/VivaLaLibertadApplication.java
@@ -0,0 +1,13 @@
+package kz.t4jgat.vivalalibertad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaLaLibertadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaLaLibertadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/t4jgat/vivalalibertad/VivaLaLibertadApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.t4jgat.vivalalibertad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaLaLibertadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
